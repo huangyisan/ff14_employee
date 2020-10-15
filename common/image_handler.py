c66e6d9158b946a9666fc1849d0f3126c6e7a134
huangyisan <anonymousyisan@gmail.com>
2020-10-16 01:34:12 +0800

    update

---
+++ common/image_handler.py
@@ -0,0 +1,24 @@
+import pyautogui
+import time
+time.sleep(5)
+# pyautogui.screenshot('Part.png',region=(722,377,1294,668))
+#
+#
+# Part = pyautogui.locateOnScreen('bell.png', confidence=0.9)
+#
+# print(Part)
+# x,y = pyautogui.center(Part)
+# print(pyautogui.center(Part))
+#
+# # 2秒钟鼠标移动到坐标为100,100的位置
+# pyautogui.moveTo(x=x, y=y, duration=1)
+# # pyautogui.moveTo(x=1149, y=471, duration=1)
+#
+# time.sleep(2)
+# pyautogui.rightClick()
+
+pyautogui.press(']')
+time.sleep(1)
+pyautogui.press(']')
+
+
